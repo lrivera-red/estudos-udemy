e42dbb866e33999af7f502c1fd8692f6e052ee12
lrivera-red <lgsrivera21@gmail.com>
2023-01-19 18:35:49 -0300

    Estudos cap 2

---
+++ src/main/java/br/estudos/domain/repository/ItemsPedido.java
@@ -0,0 +1,7 @@
+package br.estudos.domain.repository;
+
+import io.github.dougllasfps.domain.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemsPedido extends JpaRepository<ItemPedido, Integer> {
+}
---
+++ src/main/java/br/estudos/domain/repository/Produtos.java
@@ -0,0 +1,7 @@
+package br.estudos.domain.repository;
+
+import io.github.dougllasfps.domain.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produtos extends JpaRepository<Produto,Integer> {
+}
